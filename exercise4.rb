236a623e13862146ac60c150d1801a8370c3df15
John Howie <jkhowie63@gmail.com>
2016-08-09 10:59:31 -0400

    Exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,11 @@
+100.times do |num|
+  if (num % 3 == 0) && (num % 5 == 0)
+    puts "Bitmaker"
+  elsif num % 3 == 0
+    puts "Bit"
+  elsif num % 5 == 0
+    puts "Maker"
+  else
+    puts num
+  end
+end
