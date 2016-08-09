b9a774ac4a7d39c33c0f3fe5faea126b38a0bea0
John Howie <jkhowie63@gmail.com>
2016-08-09 10:25:18 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+puts "A good tip for a $55 dolar meal would be #{55 * 0.2}"
+
+puts "Adding a number to a string " + 15.to_s
+
+puts "The result of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
